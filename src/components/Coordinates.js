69d2a90f579be801900d7b267edb774ff62763b4
Tomixer <tomix.klokar@gmail.com>
2023-11-05 11:21:34 +0100

    api

--- src/components/Coordinates.js
+++ src/components/Coordinates.js
@@ -1,7 +1,23 @@
-import {useState} from "react"
+import {useState,useEffect} from "react"
+
+const url="http://api.open-notify.org/iss-now.json"
 
 const Coordinates = () => {
     const [loading,setLoading]=useState(true)
+    const [cooordinates,setCoordinates]=useState({})
+
+    const getCoordinates=async()=>{
+        const response= await fetch(url)
+        const issCoordinates= await response.json()
+        setLoading(false)
+        setCoordinates(issCoordinates)
+    }
+
+    useEffect(()=>{
+        getCoordinates()
+    },[])
+
+
   return <div>
         <div>{loading?"loading...":"data"}</div>
     </div>
